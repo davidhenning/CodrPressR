cca4d7d5bbdc580794a98c7a3fe42a0f707aeb99
David Henning <madcat.me@gmail.com>
2012-11-18 00:26:32 +0100

    show single post

--- lib/codrpress/actions/weblog.rb
+++ lib/codrpress/actions/weblog.rb
@@ -8,6 +8,14 @@ module Codrpress
                     @posts = Post.latest.limit(10)
                     slim :index
                 end
+
+                get '/:year/:month/:day/:slug/?' do |year, month, day, slug|
+                    @posts = Post.
+                        for_day(year.to_i, month.to_i, day.to_i).
+                        where(:slugs => slug).limit(1)
+
+                    slim :index
+                end
             end
         end
     end
--- lib/codrpress/post.rb
+++ lib/codrpress/post.rb
@@ -6,10 +6,34 @@ module Codrpress
         # basic data
         field :title, :type => String
         field :body,  :type => String
+        field :body_html, :type => String
         field :slugs, :type => Array, :default => []
 
         scope :published, where(:status => :published)
         scope :posts, where(:published_at.ne => nil)
+        scope :for_year, lambda { |year| d = Date.new(year) ; where(:published_at => (d.beginning_of_year)..(d.end_of_year)) }
+        scope :for_month, lambda { |year, month| d = Date.new(year, month) ; where(:published_at => (d.beginning_of_month)..(d.end_of_month)) }
+        scope :for_day, lambda { |year, month, day| d = Date.new(year, month, day) ; where(:published_at => (d.beginning_of_day)..(d.end_of_day)) }
         scope :latest, lambda { published.posts.desc(:published_at) }
+
+        def year
+            published_at.year
+        end
+
+        def month
+            published_at.month
+        end
+
+        def day
+            published_at.day
+        end
+
+        def slug
+            slugs.try(:last)
+        end
+
+        def url
+            "/#{sprintf '%04d', year}/#{sprintf '%02d', month}/#{sprintf '%02d', day}/#{slug}/"
+        end
     end
 end
