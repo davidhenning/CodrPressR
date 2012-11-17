972c2e70987698f48df8c20dcfec280aca4c1287
David Henning <madcat.me@gmail.com>
2012-11-18 00:25:37 +0100

    added public folder

--- lib/codrpress/app.rb
+++ lib/codrpress/app.rb
@@ -2,6 +2,8 @@ require 'codrpress/actions/weblog'
 
 module Codrpress
     class App < Sinatra::Base
+        set :public_folder, File.expand_path('../../public/', __FILE__)
+
         include Codrpress::Actions::Weblog
     end
 end
